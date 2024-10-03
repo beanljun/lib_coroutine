4da7bdd7359d2f555719c0446702059dafc74533
beanljun <beannan0108@gmail.com>
2024-10-03 23:24:35 +0800

    add http

---
+++ sylar/http/include/http_server.h
@@ -0,0 +1,60 @@
+/**
+ * @file http_server.h
+ * @brief HTTP服务器封装
+ * @author beanljun
+ * @date 2024-10-02
+ */
+
+#ifndef __HTTP_SERVER_H__
+#define __HTTP_SERVER_H__
+
+#include "../../net/include/tcp_server.h"
+#include "http_session.h"
+#include "servlet.h"
+
+namespace sylar {
+namespace http {
+
+/**
+ * @brief HTTP服务器类
+ */
+class HttpServer : public TcpServer {
+public:
+    /// 智能指针类型
+    typedef std::shared_ptr<HttpServer> ptr;
+
+    /**
+     * @brief 构造函数
+     * @param[in] keepalive 是否长连接
+     * @param[in] worker 工作调度器
+     * @param[in] accept_worker 接收连接调度器
+     */
+    HttpServer(bool keepalive = false
+               ,sylar::IOManager* worker = sylar::IOManager::GetThis()
+               ,sylar::IOManager* io_worker = sylar::IOManager::GetThis()
+               ,sylar::IOManager* accept_worker = sylar::IOManager::GetThis());
+
+    /**
+     * @brief 获取ServletDispatch
+     */
+    ServletDispatch::ptr getServletDispatch() const { return m_dispatch;}
+
+    /**
+     * @brief 设置ServletDispatch
+     */
+    void setServletDispatch(ServletDispatch::ptr v) { m_dispatch = v;}
+
+    virtual void setName(const std::string& v) override;
+protected:
+    virtual void handleClient(Socket::ptr client) override;
+private:
+    /// 是否支持长连接
+    bool m_isKeepalive;
+    /// Servlet分发器
+    ServletDispatch::ptr m_dispatch;
+};
+
+}
+}
+
+#endif
